48b6834fe00537c77b2dbdaadd064f1a3951ce03
zabarik21 <mityafara426@gmail.com>
2022-05-27 19:23:42 +0500

    init

---
+++ TinderApp/Controllers/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  TinderApp
+//
+//  Created by Timofey on 27/5/22.
+//
+
+import UIKit
+
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+    
+}
---
+++ TinderApp/Controllers/MessagerViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MessagerViewController.swift
+//  TinderApp
+//
+//  Created by Timofey on 27/5/22.
+//
+
+import Foundation
